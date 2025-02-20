ea81f6f15c2de5f780be7a4788cf1ddd5adf7d35
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2025-02-20 17:29:58 -0300

    Começando o codigo de verdade

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+let bodyWeight = document.querySelector('#kilogramas')
+let Height = document.querySelector('#altura')
+let Age = document.querySelector('#age')
+
+const Calcular = document.querySelector('#calcular').addEventListener("click", calcularGET)
+let Resultado = 0
+
+const activityLevel = {
+    sendetario: 1.2,
+    levemente_ativo: 1.375,
+    moderamente_ativo: 1.55,
+    muito_ativo: 1.725,
+    extremamente_ativo: 1.9
+}
+
+
+function calcularGET() {
+    
+}
